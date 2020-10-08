0466593ad07942d93cc203696e5024aa110c9b68
linjiahuizzz <1161640194@qq.com>
2020-10-08 18:32:08 +0800

    项目初始化

---
+++ src/main/com/examination/Main.java
@@ -0,0 +1,9 @@
+package main.com.examination;
+
+import main.com.examination.view.MainUI;
+
+public class Main {
+    public static void main(String[] args) {
+        MainUI exercise = new MainUI();
+    }
+}
---
+++ src/main/com/examination/commons/Operator.java
@@ -0,0 +1,41 @@
+package main.com.examination.commons;
+
+/**
+ * @author Ming
+ * @date 2020/3/27 - 0:35
+ * @describe
+ */
+public enum Operator {
+
+    /**
+     * 运算符
+     */
+    PLUS_SIGN("加号","+","+"),
+    MINUS_SIGN("减号","-","-"),
+    MULTIPLIED_SIGN("乘以号","*","*"),
+    DIVISION_SIGN("除以号","÷","/"),
+    EQUAL_SIGN("等于号","=","=");
+
+    private String name;
+    private String express;
+    private String calculation;
+
+    Operator(String name, String express, String calculation) {
+        this.name = name;
+        this.express = express;
+        this.calculation = calculation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExpress() {
+        return express;
+    }
+
+    public String getCalculation() {
+        return calculation;
+    }
+
+}
